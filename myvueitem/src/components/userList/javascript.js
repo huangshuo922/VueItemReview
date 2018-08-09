97c6c6156703ea9a641c4fc683592acd67db552f
huangshuo922 <793371210@qq.com>
2018-08-09 21:29:44 +0800

    完成分页

--- myvueitem/src/components/userList/javascript.js
+++ myvueitem/src/components/userList/javascript.js
@@ -4,25 +4,26 @@ export default {
     },
     data (){
         return{
-            pagenum: 1,
-            pagesize: 10,
-            tableData:[]
+            pagesize: 5,
+            tableData:[],
+            currentPage: 1,
+            total:10
         }
     },
     methods: {
         renderList: function(){
             this.$axios.get('http://localhost:8888/api/private/v1/users', {
                 params: {
-                    pagenum:this.pagenum,
-                    pagesize:this.pagesize
+                    pagenum:this.currentPage,
+                    pagesize:this.pagesize,
+                
                 }
               })
               .then( (response) => {
+                this.total=response.data.data.total
                 // console.log(response);
-                
-                
                 this.tableData = response.data.data.users
-
+                console.log(response.data.data)
                 // for(var i=0;i<userListArr.length;i++){
                 //     // console.log([this.tableData])
                 //     // this.tableData[this.tableData.length] = {userName:'',eMail:'',phone:''};
@@ -35,6 +36,16 @@ export default {
               .catch( (error) => {
                 console.log(error);
               });
+        },
+        handleSizeChange(val) {
+            this.pagesize = val;
+            this.renderList();
+            console.log(`每页 ${val} 条`);
+          },
+        handleCurrentChange(val) {
+            this.currentPage = val
+            this.renderList();
+        console.log(`当前页: ${val}`);
         }
     }
 }
